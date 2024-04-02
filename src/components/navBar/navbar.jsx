ed48d796ad255e5b87edf375d28cac2f10aee2c7
george827 <kinyanjuigeorge827@gmail.com>
2024-04-02 22:58:23 +0300

    Style nav component

--- src/components/navBar/navbar.jsx
+++ src/components/navBar/navbar.jsx
@@ -6,26 +6,27 @@ import bell from "../../assets/bell.svg";
 import "./navbar.css";
 
 const NavBar = () => {
-    return (
-        <nav className="navbar">
-            <div className="logo-container">
-                <img src={logo} alt="WeTeach Logo" className="teachLogo"/>
-                <h1 className="weteach-text">Weteach</h1>
-            </div>
-            <div className="welcome">
-                <p>hello 🖐</p>
-                <h5>Welcome back</h5>
-                <div className="notification">
-                    <img src={bell} alt="bell" className="bell" />
-                </div>
-            </div>
-            <div className="profile">
-                <img src={profilePic} alt="Profile" className="profilePic"/> 
-                <h5 className="profileName">Peter Njenga</h5>              
-            </div>
-        
-        </nav>
-    );
-    };
+  return (
+    <nav className="navbar">
+      <div className="logo-container">
+        <img src={logo} alt="WeTeach Logo" className="teachLogo" />
+        <h1 className="weteach-text">Weteach</h1>
+      </div>
+      <div className="welcome">
+        <div>
+          <p>hello 🖐</p>
+          <h5>Welcome back</h5>
+        </div>
+        <div className="notification">
+          <img src={bell} alt="bell" className="bell" />
+        </div>
+      </div>
+      <div className="profile">
+        <img src={profilePic} alt="Profile" className="profilePic" />
+        <h5 className="profileName">Peter Njenga</h5>
+      </div>
+    </nav>
+  );
+};
 
 export default NavBar;
