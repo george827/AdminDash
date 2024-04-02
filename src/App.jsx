04609925bfdec2b5920781ae4cad8c60b47570cd
george827 <kinyanjuigeorge827@gmail.com>
2024-04-02 15:30:49 +0300

    Initialize with react

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+       <P>hello world</P>
+        
+      </div>  
+    </>
+  )
+}
+
+export default App
